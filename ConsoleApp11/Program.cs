ab8e6b9d8dbcc92c2474f264d9bb74ae5d8d9a33
Matúš <matusskor@gmail.com>
2020-11-11 20:43:06 +0100

    Add project files.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ConsoleApp11
+{
+
+    class Book
+    {
+        public string title;
+        int pages;
+        public void F(int a , string b)
+        {
+            pages = a;
+            title = b;
+            Console.WriteLine(title + " " +pages);
+        }
+    }
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book LOTR = new Book();
+            
+            LOTR.F(5, "Abeceda");
+            
+        }
+    }
+   
+}
